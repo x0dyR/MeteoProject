b9f794d70c77097467bf657fe7b92396daea3953
x0dyR <manoshki.dan4ik@gmail.com>
2025-03-30 16:05:28 +0500

    .

--- src/services/sensorService.js
+++ src/services/sensorService.js
@@ -1,30 +1,9 @@
-const SensorData = require('../model/sensorData');
-const sensor = require('node-dht-sensor');
-
+// src/services/sensorService.js
 exports.readSensorData = async () => {
-  return new Promise((resolve, reject) => {
-    // Ждём 3 секунды
-    setTimeout(() => {
-      sensor.read(11, 4, (err, temperature, humidity) => {
-        if (err) {
-          return reject(err);
-        }
-        // Выводим в консоль исходные данные для проверки
-        console.log(`Сырые данные: температура = ${temperature}, влажность = ${humidity}`);
-
-        // Если значения кажутся перепутанными, можно поменять их местами:
-        // const data = new SensorData(
-        //   humidity.toFixed(1), // предполагаемая температура
-        //   temperature.toFixed(1) // предполагаемая влажность
-        // );
-
-        // Если всё верно, оставляем порядок таким:
-        const data = new SensorData(
-          temperature.toFixed(1),
-          humidity.toFixed(1)
-        );
-        resolve(data);
-      });
-    }, 3000);
-  });
-};
+    // Возвращаем статичные тестовые данные
+    return {
+      temperature: 23.5,
+      humidity: 45
+    };
+  };
+  
