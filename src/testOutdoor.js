4ae036dd9e87b9a24c363bc0938f40a0de6260f2
x0dyR <manoshki.dan4ik@gmail.com>
2025-03-30 16:02:15 +0500

    test script

---
+++ src/testOutdoor.js
@@ -0,0 +1,9 @@
+const outdoorWeatherService = require('./src/services/outdoorWeatherService');
+
+outdoorWeatherService.getSensorData()
+  .then(data => {
+    console.log('Outdoor data:', data);
+  })
+  .catch(err => {
+    console.error('Error in outdoorWeatherService:', err);
+  });
