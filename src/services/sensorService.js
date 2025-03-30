345f747e4459d914ee416442feb20009feaa034e
x0dyR <manoshki.dan4ik@gmail.com>
2025-03-30 18:21:21 +0500

    .

--- src/services/sensorService.js
+++ src/services/sensorService.js
@@ -3,17 +3,22 @@ const sensor = require('node-dht-sensor');
 
 exports.readSensorData = async () => {
   return new Promise((resolve, reject) => {
-    // Явная инициализация датчика
-    if (!sensor.initialize(11, 4)) {
+    // Явная инициализация датчика (тип 11 для DHT11, пин 4)
+    const initialized = sensor.initialize(11, 4);
+    console.log('Инициализация датчика:', initialized);
+    if (!initialized) {
       return reject(new Error('failed to initialize sensor'));
     }
-    sensor.read(11, 4, (err, temperature, humidity) => {
-      if (err) {
-        console.error('Ошибка чтения датчика:', err);
-        return reject(new Error('failed to read sensor: ' + err));
-      }
-      console.log('Данные с датчика:', { temperature, humidity });
-      resolve({ temperature, humidity });
-    });
-  },3000);
+    // Задержка 2 секунды для стабилизации датчика
+    setTimeout(() => {
+      sensor.read(11, 4, (err, temperature, humidity) => {
+        if (err) {
+          console.error('Ошибка чтения датчика:', err);
+          return reject(new Error('failed to read sensor: ' + err));
+        }
+        console.log('Данные с датчика:', { temperature, humidity });
+        resolve({ temperature, humidity });
+      });
+    }, 2000);
+  });
 };
