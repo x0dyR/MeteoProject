78535c02256b313f5bc39eb47eb446234691822b
x0dyR <manoshki.dan4ik@gmail.com>
2025-03-30 19:53:26 +0500

    .

--- src/testSensor.js
+++ src/testSensor.js
@@ -1,20 +1,7 @@
-const sensor = require('node-dht-sensor-rp5');
+var sensor = require("node-dht-sensor");
 
-// Инициализируем датчик
-const initialized = sensor.initialize(11, 4);
-console.log('Инициализация датчика:', initialized);
-
-if (!initialized) {
-  console.error('Ошибка инициализации датчика');
-  process.exit(1);
-}
-
-setTimeout(() => {
-  sensor.read(11, 4, (err, temperature, humidity, isValid, errors) => {
-    if (err) {
-      console.error('Ошибка чтения датчика:', err);
-    } else {
-      console.log('Считанные данные:', { temperature, humidity, isValid, errors });
-    }
-  });
-}, 3000);
+sensor.read(11, 4, function(err, temperature, humidity) {
+  if (!err) {
+    console.log(`temp: ${temperature}°C, humidity: ${humidity}%`);
+  }
+});
