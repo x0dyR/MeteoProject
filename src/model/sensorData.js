a3c7227d7b152cfb03dca2863c6b99cacfae92e0
x0dyR <manoshki.dan4ik@gmail.com>
2025-03-30 17:29:56 +0500

    .

--- src/model/sensorData.js
+++ src/model/sensorData.js
@@ -1,10 +1,9 @@
 class SensorData {
     constructor(temperature, humidity) {
-      this.temperature = temperature;
-      this.humidity = humidity;
-      this.timestamp = new Date();
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.timestamp = new Date();
     }
-  }
-  
-  module.exports = SensorData;
-  
+}
+
+module.exports = SensorData;
--- src/services/sensorService.js
+++ src/services/sensorService.js
@@ -3,6 +3,10 @@ const sensor = require('node-dht-sensor');
 
 exports.readSensorData = async () => {
   return new Promise((resolve, reject) => {
+    // Явная инициализация датчика
+    if (!sensor.initialize(11, 4)) {
+      return reject(new Error('failed to initialize sensor'));
+    }
     sensor.read(11, 4, (err, temperature, humidity) => {
       if (err) {
         console.error('Ошибка чтения датчика:', err);
