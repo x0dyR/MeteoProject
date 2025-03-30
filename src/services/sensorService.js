63f9841bb359747d08fd524f4e78afe0e9ff4349
x0dyR <manoshki.dan4ik@gmail.com>
2025-03-30 14:04:04 +0500

    sensorService update

--- src/services/sensorService.js
+++ src/services/sensorService.js
@@ -3,14 +3,19 @@ const sensor = require('node-dht-sensor');
 
 exports.readSensorData = async () => {
   return new Promise((resolve, reject) => {
-    // sensor.read(тип датчика, номер пина, callback)
-    sensor.read(22, 4, (err, temperature, humidity) => {
-      if (err) {
-        return reject(err);
-      }
-      // Округляем значения до одного знака после запятой
-      const data = new SensorData(temperature.toFixed(1), humidity.toFixed(1));
-      resolve(data);
-    });
+    // Ждём 3 секунды
+    setTimeout(() => {
+      sensor.read(11, 4, (err, temperature, humidity) => {
+        if (err) {
+          return reject(err);
+        }
+        // Округляем значения до одного знака
+        const data = new SensorData(
+          temperature.toFixed(1),
+          humidity.toFixed(1)
+        );
+        resolve(data);
+      });
+    }, 3000);
   });
 };
