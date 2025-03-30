7021f04bea29187a9b245ee0c9faf8a66602a6e1
x0dyR <manoshki.dan4ik@gmail.com>
2025-03-30 15:00:37 +0500

    .

--- src/services/outdoorWeatherService.js
+++ src/services/outdoorWeatherService.js
@@ -1,28 +1,14 @@
-const axios = require('axios');
 const cheerio = require('cheerio');
 
-exports.getOutdoorTemperature = async () => {
-  try {
-    const { data: html } = await axios.get('https://www.gismeteo.kz/weather-almaty-5205/');
-    const $ = cheerio.load(html);
-    
-    // Ищем div с классом "weather-value", затем внутри него элемент с классом "temperature-value"
-    const tempText = $('div.weather-value')
-      .find('.temperature-value')
-      .first()
-      .text()
-      .trim();
-    
-    // Преобразуем строку в число (убираем лишние символы, если они есть)
-    const temperature = parseFloat(tempText.replace(/[^\d\.\-]/g, ''));
-    
-    if (isNaN(temperature)) {
-      throw new Error('Не удалось распознать температуру с сайта');
-    }
-    
-    return temperature;
-  } catch (error) {
-    console.error('Ошибка при получении внешней температуры:', error.message);
-    throw error;
-  }
-};
+// Здесь вставьте HTML, например, в переменную html
+const html = `<!doctype html> ... </html>`; // Ваш HTML код
+
+const $ = cheerio.load(html);
+
+// Ищем первый элемент <temperature-value> внутри div.weather-value
+const temperature = $('div.weather-value')
+  .find('temperature-value')
+  .first()
+  .attr('value');
+
+console.log('Температура:', temperature);
