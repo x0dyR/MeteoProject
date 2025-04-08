2cd28507feebb8246fc223b9063acd1192aba3f2
x0dyR <manoshki.dan4ik@gmail.com>
2025-04-08 23:03:55 +0500

    .

--- src/services/sensorService.js
+++ src/services/sensorService.js
@@ -3,13 +3,6 @@ const sensor = require('node-dht-sensor');
 
 exports.readSensorData = async () => {
   return new Promise((resolve, reject) => {
-    // Инициализация датчика DHT11 на пине 4
-    const initialized = sensor.initialize(11, 4);
-    console.log('[DEBUG] Инициализация датчика (node-dht-sensor):', initialized);
-    if (!initialized) {
-      return reject(new Error('failed to initialize sensor'));
-    }
-    // Задержка в 2000 мс для стабилизации датчика (можно увеличить, если нужно)
     setTimeout(() => {
       sensor.read(11, 4, (err, temperature, humidity) => {
         if (err) {
