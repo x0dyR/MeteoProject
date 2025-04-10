b54da85d8a37791044cb0b574c790bc9820a755c
x0dyR <manoshki.dan4ik@gmail.com>
2025-04-10 14:28:27 -0900

    final!

--- src/controllers/sensorController.js
+++ src/controllers/sensorController.js
@@ -7,10 +7,13 @@ const adviceService = require('../services/adviceService');
 
 exports.getSensorData = async (req, res) => {
   try {
-    const sensorData = await sensorService.readSensorData();
+    // Используем getValidSensorData вместо readSensorData для получения актуальных данных
+    const sensorData = await sensorService.getValidSensorData();
     const outdoorData = await outdoorWeatherService.getSensorData();
+
     // Предсказываем индекс комфорта с помощью ML
     const comfortIndex = await mlService.predictComfort(sensorData, outdoorData);
+
     // Получаем рекомендации с учетом установленных комфортных значений
     const recommendation = adviceService.getImprovementAdvice(sensorData, outdoorData, comfortIndex);
 
